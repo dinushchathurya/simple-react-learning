428aefb8fa5878e7d06b8f6434aa8caee86387d4
dinushchathurya <dinushchathurya21@gmail.com>
2021-05-20 21:34:06 +0530

    feat: peering Employee component into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
-import Counter  from './Counter';
+import Employee from './Employee';
+
+const employeeInfo = [
+  {
+    firstName: "Jack",
+    lastName: "Smith",
+    age: "20",
+    employeeId: "1"
+  },
+  {
+    firstName: "Ann",
+    lastName: "Mary",
+    age: "19",
+    employeeId: "2"
+  }
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <Counter />
+        { employeeInfo.map((employee)=> {
+          return (
+            <Employee key={employee.employeeId} {...employee} />
+          );
+        })}
       </header>
     </div>
   );
